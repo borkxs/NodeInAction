a094bcbafde75711194c120c4e5eb411c042788c
Erik Michaelson <erik.michaelson@gmail.com>
2015-06-14 16:43:45 -0500

    3.1.2 module exports, constructor

--- test-currency.js
+++ test-currency.js
@@ -1,7 +1,10 @@
-var currency = require('./currency')
+var Currency = require('./currency')
+var canadianDollar = 0.91
 
+var currency = newCurrency(canadianDollar)
 console.log('50 Canadian dollars equals this amount of US dollars:')
-console.log(currency.canadianToUS(50))
+console.log(currency.canadianToUs(50))
 
+var currency2 = newCurrency(canadianDollar)
 console.log('30 US dollars equals this amount of Canadian dollars:')
-console.log(currency.USToCanadian(30))
+console.log(currency2.USToCanadian(30))
