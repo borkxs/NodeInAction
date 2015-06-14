44d287c589aabf37c2df68c0ef724efa3eb28eba
Erik Michaelson <erik.michaelson@gmail.com>
2015-06-14 01:05:12 -0500

    static file server fixes

--- server.js
+++ server.js
@@ -1,43 +1,61 @@
 var http = require('http'),
-	fs = require('fs'),
-	path = require('path'),
-	mime = require('mime')
+    fs = require('fs'),
+    path = require('path'),
+    mime = require('mime')
 
 var cache = {}
 
-function send404(response) {
-	res.writeHead(404, {
-		'ContentType': 'text/plain'
-	})
-	res.write('Error 404: resource not found')
-	response.end()
+var server = http.createServer(function (request, response) {
+    var filePath = false,
+        absPath
+
+    if (request.url == "/")
+        filePath = 'public/index.html'
+    else
+        filePath = 'public' + request.url
+
+    absPath = './' + filePath
+    serveStatic(response, cache, absPath)
+})
+
+server.listen(3000, function () {
+    console.log("Server listening on port 3000.")
+})
+
+function serveStatic(response, cache, absPath) {
+    var fileInCache = cache[absPath]
+
+    if (fileInCache)
+        sendFile(response, absPath, fileInCache)
+    else
+        fs.exists(absPath, function(exists) {
+            if (exists)
+                fs.readFile(absPath, function(err, data) {
+                    if (err)
+                        send404(response)
+                    else {
+                        cache[absPath] = data
+                        sendFile(response, absPath, data)
+                    }
+                })
+            else
+                send404(response)
+        })
+
 }
 
 function sendFile(response, filePath, fileContents) {
-	response.writeHead(200, {
-		'Content-Type': mime.lookup(path.basename(filePath))
-	});
-	response.end(fileContents)
+    console.log(mime.lookup(path.basename(filePath)))
+    response.writeHead(200, {
+        'Content-Type': mime.lookup(path.basename(filePath))
+    })
+    response.end(fileContents)
 }
 
-function serverStatc(response, cache, absPath) {
-	var fileInCache = cache[absPath]
-
-	if (fileInCache)
-		sendFile(response, absPath, fileInCache)
-	else
-		fs.exists(absPath, function(exists) {
-			if (exists)
-				fs.readFile(absPath, function(err, data) {
-					if (err)
-						send404(response)
-					else {
-						cache[absPath] = data
-						sendFile(response, cache, data)
-					}
-				})
-			else
-				send404(response)
-		})
-
+function send404(response) {
+    response.writeHead(404, {
+        'ContentType': 'text/plain'
+    })
+    response.write('Error 404: resource not found')
+    response.end()
 }
