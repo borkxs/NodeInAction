ca3c9b163863a2f9f04507c22d041e365ea5c595
Erik Michaelson <erik.michaelson@gmail.com>
2015-06-27 17:32:17 -0500

    add welcome message with guest number

--- NIACH3/3.2.2/echo_server.js
+++ NIACH3/3.2.2/echo_server.js
@@ -6,6 +6,10 @@ channel.clients = {}
 channel.subscriptions = {}
 
 channel.on('join', function(id, client) {
+    var welcome = "Welcome!\n"
+            + 'Guests online: ' + this.listeners('broadcast').length
+    client.write(welcome + "\n")
+    
     this.clients[id] = client
     this.subscriptions[id] = function(senderId, message) {
         if (id != senderId)
