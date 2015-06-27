0e6c81af74894c0148f73fbf7a3a965c9771e80c
Erik Michaelson <erik.michaelson@gmail.com>
2015-06-27 17:14:38 -0500

    net.createServer effectively is the connect event listener

--- NIACH3/3.2.2/echo_server.js
+++ NIACH3/3.2.2/echo_server.js
@@ -1,24 +1,28 @@
-var events = require('events')
+var events = require('events'),
     net = require('net')
 
 var channel = new events.EventEmitter()
 channel.clients = {}
 channel.subscriptions = {}
 
+channel.on('join', function(id, client) {
+    this.clients[id] = client
+    this.subscriptions[id] = function(senderId, message) {
+        if (id != senderId)
+            this.clients[id].write(message)
+    }
+    this.on('broadcast', channel.subscriptions[id])
+})
+
+// subscribe client to broadcasts
+channel.on('leave', function(id) {
+    channel.removeListener('broadcast', this.subscriptions[id])
+    channel.emit('broadcast', id, id + " has left the chat.\n")
+})
+
 var server = net.createServer(function(client) {
     var id = client.remoteAddress + ':' + client.remotePort
-
-    // setup client handler
-    channel.clients[id] = client
-    channel.subscriptions[id] = function(senderId, message) {
-        if ( id != senderId )
-            channel.clients[id].write(message)
-    }
-
-    // subscribe client to broadcasts
-    channel.on('broadcast', channel.subscriptions[id])
-
-    // broadcast data from client
+    channel.emit('join', id, client)
     client.on('data', function(data) {
         channel.emit('broadcast', id, data.toString())
     })
