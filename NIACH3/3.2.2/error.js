664d47a8bf4b41063b0ee792636b1bb9c00fe56e
Erik Michaelson <erik.michaelson@gmail.com>
2015-06-27 17:32:42 -0500

    global exception handler example (appears to not work)

--- NIACH3/3.2.2/error.js
+++ NIACH3/3.2.2/error.js
@@ -5,3 +5,16 @@ myEmitter.on('error', function(err) {
     console.log('ERROR: ' + err.message)
 })
 myEmitter.emit('error', new Error('Something is wrong.'))
+
+// If an error type event is emitted without an error object supplied as the 
+// second argument, a stack trace will indicate an "Uncaught, unspcified 'error' 
+// event" error, and your application will halt. There is a deprecated method
+// you can use to deal with this error--you can define your own response by 
+// defining a global handler using the following code:
+
+// process.on('uncaughtException', function(err){
+//     console.error(err.stack)
+//     process.exit(1)
+// })
+
+myEmitter.emit('error', 'asdf')
