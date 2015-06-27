b546d1be95c6c63630795ed48e975a713332edd4
Erik Michaelson <erik.michaelson@gmail.com>
2015-06-27 18:04:37 -0500

    3.15 using an anonymous function to preserve a global variable's value. IIFE's are an interesting problem for the anti-semicolon view

--- NIACH3/3.2.3/scope.js
+++ NIACH3/3.2.3/scope.js
@@ -4,8 +4,10 @@ function asyncFunction(callback) {
 
 var color = 'blue'
 
-asyncFunction(function() {
-    console.log('The color is ' + color)
-})
+;(function (color) {
+    asyncFunction(function() {
+        console.log('The color is ' + color)
+    })
+}(color))
 
 color = 'green'
