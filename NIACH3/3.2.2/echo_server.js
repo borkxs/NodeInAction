abce32ef4b63bed3c3edd181d7da66bd8af054d6
Erik Michaelson <erik.michaelson@gmail.com>
2015-06-27 16:23:56 -0500

    fixes(?) for 3.11

--- NIACH3/3.2.2/echo_server.js
+++ NIACH3/3.2.2/echo_server.js
@@ -1,26 +1,25 @@
 var events = require('events')
-    net = require('net')
+net = require('net')
 
 var channel = new events.EventEmitter()
 channel.clients = {}
 channel.subscriptions = {}
 
-channel.on('join', function(id, client) {
-    this.clients[id] = client
-    this.subscriptions[id] = function(senderId, message ) {
-        if (id != senderId) {
-            this.clients[id].write(mesage)
-        }
-    }
-    this.on('broadcast', this.subscriptions[id])
-})
-
 var server = net.createServer(function(client) {
     var id = client.remoteAddress + ':' + client.remotePort
-    client.on('connect', function() {
-        channel.emit('join', id, client)
-    })
-    client.on('data', function(data){
+
+    // setup client handler
+    channel.clients[id] = client
+    channel.subscriptions[id] = function(senderId, message) {
+        if ( id != senderId )
+            channel.clients[id].write(message)
+    }
+
+    // subscribe client to broadcasts
+    channel.on('broadcast', channel.subscriptions[id])
+
+    // broadcast data from client
+    client.on('data', function(data) {
         channel.emit('broadcast', id, data.toString())
     })
 })
