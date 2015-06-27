cdc7a20b50cb030cea994439c6297943b917f87c
Erik Michaelson <erik.michaelson@gmail.com>
2015-06-27 16:22:40 -0500

    3.11 a simple publish/subscribe system using an event emitter (does not appear to work, seems like client connect never gets fired

--- NIACH3/3.2.2/echo_server.js
+++ NIACH3/3.2.2/echo_server.js
@@ -1,11 +1,29 @@
-var net = require('net')
+var events = require('events')
+    net = require('net')
 
-var server = net.createServer(function(socket) {
-    socket.once('data', function(data) {
-        socket.write(data)
-    })
+var channel = new events.EventEmitter()
+channel.clients = {}
+channel.subscriptions = {}
+
+channel.on('join', function(id, client) {
+    this.clients[id] = client
+    this.subscriptions[id] = function(senderId, message ) {
+        if (id != senderId) {
+            this.clients[id].write(mesage)
+        }
+    }
+    this.on('broadcast', this.subscriptions[id])
 })
 
+var server = net.createServer(function(client) {
+    var id = client.remoteAddress + ':' + client.remotePort
+    client.on('connect', function() {
+        channel.emit('join', id, client)
+    })
+    client.on('data', function(data){
+        channel.emit('broadcast', id, data.toString())
+    })
+})
 server.listen(8000)
 
-// run telnet 127.0.0.1 8888
+// run telnet 127.0.0.1 8000
