a72aaf4d3e2d2cd79d0899893f92d1f08800bb3d
Erik Michaelson <erik.michaelson@gmail.com>
2015-06-27 16:08:03 -0500

    3.10 using the once method to respond to a single event

--- NIACH3/3.2.2/echo_server.js
+++ NIACH3/3.2.2/echo_server.js
@@ -1,7 +1,7 @@
 var net = require('net')
 
 var server = net.createServer(function(socket) {
-    socket.on('data', function(data) {
+    socket.once('data', function(data) {
         socket.write(data)
     })
 })
