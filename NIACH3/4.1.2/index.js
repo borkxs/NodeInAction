102314f9513599e0b8378338b4379d3a04d87568
Erik Michaelson <erik.michaelson@gmail.com>
2015-07-12 19:57:07 -0500

    4.1.4 setting status code of HTTP response

--- NIACH3/4.1.2/index.js
+++ NIACH3/4.1.2/index.js
@@ -6,9 +6,13 @@ res.removeHeader(field);
 
 var http = require('http');
 var server = http.createServer(function (req, res) {
-    var body = 'Hello World';
-    res.setHeader('Content-Length', 'text/plain');
+    var url = 'http://google.com';
+    var body = '<p>Redirecting to <a href="' + url + '">' + url + '</a></p>';
+    
+    res.setHeader('Location', url);
+    res.setHeader('Content-Length', body.length);
     res.setHeader('Content-Type', 'text/plain');
+    res.statusCode = 302;
     res.end(body);
 });
 server.listen(3000);
