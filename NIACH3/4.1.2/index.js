9d70c73bb0b179ad31327e2828d1105dae3be6a7
Erik Michaelson <erik.michaelson@gmail.com>
2015-07-12 19:43:37 -0500

    4.1.3 request headers

--- NIACH3/4.1.2/index.js
+++ NIACH3/4.1.2/index.js
@@ -1,5 +1,14 @@
+/*
+res.setHeader(field, value);
+res.getHeader(field);
+res.removeHeader(field);
+*/
+
 var http = require('http');
 var server = http.createServer(function (req, res) {
-    res.end('Hello World');
+    var body = 'Hello World';
+    res.setHeader('Content-Length', 'text/plain');
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(body);
 });
 server.listen(3000);
